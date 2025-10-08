42830fae8fefaec7e1b5101e8bc0db4b67471ed5
james <james_d02@protonmail.com>
2025-10-08 22:39:25 +0100

    formats

--- sites/jamesdurban.com/src/content/config.ts
+++ sites/jamesdurban.com/src/content/config.ts
@@ -1,14 +1,14 @@
-import {defineCollection, z} from "astro:content";
+import { defineCollection, z } from "astro:content";
 
 export const collections = {
-    project: defineCollection({
-        schema: z.object({
-            title: z.string(),
-            github: z.string().optional(),
-            publishDate: z.coerce.date(),
-            skills: z.array(z.string()),
-            img: z.string(),
-            img_alt: z.string(),
-        }),
-    })
+  project: defineCollection({
+    schema: z.object({
+      title: z.string(),
+      github: z.string().optional(),
+      publishDate: z.coerce.date(),
+      skills: z.array(z.string()),
+      img: z.string(),
+      img_alt: z.string(),
+    }),
+  }),
 };
--- sites/jamesdurban.com/src/content/project/push-notifications.md
+++ sites/jamesdurban.com/src/content/project/push-notifications.md
@@ -14,5 +14,5 @@ github: private
 
 # Overview
 
-During my time at PayPoint, I was tasked with building a push notification service for the BBC to allow them to send 
+During my time at PayPoint, I was tasked with building a push notification service for the BBC to allow them to send
 push notifications to their users when their TVL Licensing account had a low balance.
