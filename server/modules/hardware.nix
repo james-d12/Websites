161031c29d71f4b4f126da5592e9b6bb68a5489d
james <james_d02@protonmail.com>
2025-10-09 14:21:56 +0100

    more fixes

--- server/modules/hardware.nix
+++ server/modules/hardware.nix
@@ -21,7 +21,7 @@
   networking.useDHCP = lib.mkDefault true;
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 
-  boot.initrd.availableKernelModules = [ "ahci" "xhci_pci" "virtio_pci" "virtio_scsi" "sd_mod" "sr_modg"];
+  boot.initrd.availableKernelModules = [ "ahci" "xhci_pci" "virtio_pci" "virtio_scsi" "sd_mod" "sr_mod"];
   boot.initrd.kernelModules = [];
   boot.kernelModules = [];
   boot.extraModulePackages = [];
