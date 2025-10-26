61e0ad65acd2913b92cf9cabbf94aa45b9db6df2
James <james_d02@protonmail.com>
2025-10-26 14:21:20 +0000

    feat: Improve NixOS Config to include sops-nix for adding secrets (#31)
    * fixes
    
    * Update server

--- server/config/flake.nix
+++ server/config/flake.nix
@@ -6,15 +6,18 @@
     disko.url = "github:nix-community/disko";
     disko.inputs.nixpkgs.follows = "nixpkgs";
     nixos-facter-modules.url = "github:numtide/nixos-facter-modules";
+    sops-nix.url = "github:Mic92/sops-nix";
+    sops-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, disko, nixos-facter-modules,... }:
+  outputs = { self, nixpkgs, disko, nixos-facter-modules, sops-nix, ... }:
     {
         nixosConfigurations = {
             vps = nixpkgs.lib.nixosSystem {
                 system = "x86_64-linux";
                 modules = [
                     disko.nixosModules.disko
+                    sops-nix.nixosModules.sops
                     ./configuration.nix
                 ];
             };
