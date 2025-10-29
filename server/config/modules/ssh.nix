cc53d6e87757a52ef2e0edd2f4a42a177b3afb47
james <james_d02@protonmail.com>
2025-10-29 22:37:38 +0000

    Added more sites and new user for CI operations

--- server/config/modules/ssh.nix
+++ server/config/modules/ssh.nix
@@ -9,21 +9,29 @@
         UsePAM = false;
         StrictModes = true;
         PasswordAuthentication = false;
-        AllowUsers = [ "james" ];
+        AllowUsers = [ "james" "ci" ];
         X11Forwarding = false;
     };
     extraConfig = ''
-          MaxSessions 2
+          MaxSessions 3
+          MaxAuthTries 6
+          LoginGraceTime 20
           ClientAliveInterval 300
           ClientAliveCountMax 0
-          AllowTcpForwarding yes
+          AllowTcpForwarding no
           AllowAgentForwarding no
           AllowStreamLocalForwarding no
           AuthenticationMethods publickey
+          PermitEmptyPasswords no
     '';
   };
 
-  users.users.james.openssh.authorizedKeys.keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPgiJiaS9ydBTHyc7YBei0hEyH4rspbWWFJxy73JWfVI james@USER-PC"
-  ];
+  users.users = {
+    james.openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPgiJiaS9ydBTHyc7YBei0hEyH4rspbWWFJxy73JWfVI james@USER-PC"
+    ];
+    ci.openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIxSfuVkLwOIIwyJHkIry+fNXDBtJICHnZc0ej6l6kX3 james@USER-PC"
+    ];
+  };
 }
