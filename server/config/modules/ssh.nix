223a353ed0841058dfa4e3664e0b434d781f6678
james <james_d02@protonmail.com>
2025-11-02 20:41:19 +0000

    allow sftp

--- server/config/modules/ssh.nix
+++ server/config/modules/ssh.nix
@@ -2,7 +2,7 @@
   services.openssh = {
     enable = true;
     ports = [ 51196 ];
-    allowSFTP = false;
+    allowSFTP = true;
     settings = {
         KbdInteractiveAuthentication = false;
         PermitRootLogin = "no";
