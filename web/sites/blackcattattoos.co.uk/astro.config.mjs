6ab53642071353a4887738dcabd9f34baef0dd96
james <james_d02@protonmail.com>
2025-11-12 00:30:53 +0000

    Format

--- web/sites/blackcattattoos.co.uk/astro.config.mjs
+++ web/sites/blackcattattoos.co.uk/astro.config.mjs
@@ -1,20 +1,17 @@
-import {defineConfig, passthroughImageService} from "astro/config";
+import { defineConfig, passthroughImageService } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import solidJs from "@astrojs/solid-js";
 
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-    vite: {
-        plugins: [tailwindcss()],
-    },
-    site: 'https://blackcattattoos.co.uk',
-    image: {
-        service: passthroughImageService(),
-    },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  site: "https://blackcattattoos.co.uk",
+  image: {
+    service: passthroughImageService(),
+  },
 
-    integrations: [
-        solidJs(),
-        sitemap()
-    ],
+  integrations: [solidJs(), sitemap()],
 });
