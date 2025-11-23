dd16163bccfa8ab72741c00640481798071d252d
james <james_d02@protonmail.com>
2025-11-23 12:29:54 +0000

    eslint

--- web/sites/blackcattattoos.co.uk/src/utils/Images.ts
+++ web/sites/blackcattattoos.co.uk/src/utils/Images.ts
@@ -73,7 +73,7 @@ function shuffle(array: GallerySlide[]) {
   let currentIndex = array.length;
 
   while (currentIndex != 0) {
-    let randomIndex = Math.floor(Math.random() * currentIndex);
+    const randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex--;
 
     [array[currentIndex], array[randomIndex]] = [
--- web/sites/jamesdurban.com/src/data/ProjectImages.ts
+++ web/sites/jamesdurban.com/src/data/ProjectImages.ts
@@ -1,9 +1,9 @@
-// src/data/projectImages.ts
 import atomicImg from "../assets/atomic.jpg";
 import pandaImg from "../assets/panda.png";
 import pushNotificationsImg from "../assets/push-notifications.jpg";
+import type {ImageMetadata} from "astro";
 
-export const projectImages: Record<string, any> = {
+export const projectImages: Record<string, ImageMetadata> = {
   atomic: atomicImg,
   panda: pandaImg,
   "push-notifications": pushNotificationsImg,
