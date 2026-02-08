5b48b65f62200666f03d97226f9186bb0db03616
james <james_d02@protonmail.com>
2026-02-08 21:51:51 +0000

    format

--- web/sites/thecontourclinicrichmond.co.uk/astro.config.mjs
+++ web/sites/thecontourclinicrichmond.co.uk/astro.config.mjs
@@ -1,15 +1,15 @@
-import {defineConfig, sharpImageService} from "astro/config";
+import { defineConfig, sharpImageService } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import solidJs from "@astrojs/solid-js";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-    site: "https://thecontourclinicrichmond.co.uk",
-    vite: {
-        plugins: [tailwindcss()],
-    },
-    image: {
-        service: sharpImageService(),
-    },
-    integrations: [solidJs(), sitemap()],
+  site: "https://thecontourclinicrichmond.co.uk",
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  image: {
+    service: sharpImageService(),
+  },
+  integrations: [solidJs(), sitemap()],
 });
