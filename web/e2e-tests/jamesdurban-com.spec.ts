6862bb8cac765917eac930c200a32c99d081f421
james <james_d02@protonmail.com>
2026-03-22 21:50:21 +0000

    initial playwright

---
+++ web/e2e-tests/jamesdurban-com.spec.ts
@@ -0,0 +1,7 @@
+import {test, expect} from '@playwright/test';
+
+const BASE_URL = process.env.JAMESDURBAN_BASE_URL ?? "http://localhost:35421"
+test('has title', async ({page}) => {
+    await page.goto(BASE_URL);
+    await expect(page).toHaveTitle(/James Durban/);
+});
---
+++ web/playwright.config.ts
@@ -0,0 +1,42 @@
+import {defineConfig, devices} from '@playwright/test';
+/*
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config({path: path.resolve(__dirname, '.env')});
+*/
+
+export default defineConfig({
+    testDir: '././e2e-tests',
+    fullyParallel: true,
+    forbidOnly: !!process.env.CI,
+    retries: process.env.CI ? 2 : 0,
+    workers: process.env.CI ? 1 : undefined,
+    reporter: 'html',
+    use: {
+        trace: 'on-first-retry',
+    },
+    projects: [
+        {
+            name: 'chromium',
+            use: {...devices['Desktop Chrome']},
+        },
+        {
+            name: 'firefox',
+            use: {...devices['Desktop Firefox']},
+        },
+        {
+            name: 'webkit',
+            use: {...devices['Desktop Safari']},
+        }
+    ],
+    /*
+    webServer: [
+        {
+            command: 'pnpm --filter jamesdurban.com dev',
+            url: 'http://localhost:35421',
+            timeout: 120 * 1000,
+            reuseExistingServer: !process.env.CI,
+        },
+    ]*/
+});
