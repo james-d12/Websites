d3e0d0bd5ba063b7e2d2929b49a17cddfdb783cc
james <james_d02@protonmail.com>
2026-03-25 18:41:30 +0000

    format

--- web/playwright.config.ts
+++ web/playwright.config.ts
@@ -1,4 +1,4 @@
-import {defineConfig, devices} from '@playwright/test';
+import { defineConfig, devices } from "@playwright/test";
 /*
 import dotenv from 'dotenv';
 import path from 'path';
@@ -7,35 +7,35 @@ dotenv.config({path: path.resolve(__dirname, '.env')});
 */
 
 export default defineConfig({
-    testDir: '././e2e-tests',
-    fullyParallel: true,
-    forbidOnly: !!process.env.CI,
-    retries: process.env.CI ? 2 : 0,
-    workers: process.env.CI ? 1 : undefined,
-    reporter: 'html',
-    use: {
-        trace: 'on-first-retry',
+  testDir: "././e2e-tests",
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: "html",
+  use: {
+    trace: "on-first-retry",
+  },
+  projects: [
+    {
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
     },
-    projects: [
-        {
-            name: 'chromium',
-            use: {...devices['Desktop Chrome']},
-        },
-        {
-            name: 'firefox',
-            use: {...devices['Desktop Firefox']},
-        },
-        {
-            name: 'webkit',
-            use: {...devices['Desktop Safari']},
-        }
-    ],
-    webServer: [
-        {
-            command: 'pnpm --filter jamesdurban.com dev',
-            url: 'http://localhost:35421',
-            timeout: 120 * 1000,
-            reuseExistingServer: !process.env.CI,
-        },
-    ]
+    {
+      name: "firefox",
+      use: { ...devices["Desktop Firefox"] },
+    },
+    {
+      name: "webkit",
+      use: { ...devices["Desktop Safari"] },
+    },
+  ],
+  webServer: [
+    {
+      command: "pnpm --filter jamesdurban.com dev",
+      url: "http://localhost:35421",
+      timeout: 120 * 1000,
+      reuseExistingServer: !process.env.CI,
+    },
+  ],
 });
