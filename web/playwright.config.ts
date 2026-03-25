4bec4d8e69f1b52bf66f1d45b6264bb7ac69a855
james <james_d02@protonmail.com>
2026-03-25 19:00:04 +0000

    remove firefox for now

--- web/playwright.config.ts
+++ web/playwright.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, devices } from "@playwright/test";
+import {defineConfig, devices} from "@playwright/test";
 /*
 import dotenv from 'dotenv';
 import path from 'path';
@@ -7,59 +7,55 @@ dotenv.config({path: path.resolve(__dirname, '.env')});
 */
 
 export default defineConfig({
-  testDir: "././e2e-tests",
-  fullyParallel: true,
-  forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
-  reporter: "html",
-  use: {
-    trace: "on-first-retry",
-  },
-  projects: [
-    {
-      name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
+    testDir: "././e2e-tests",
+    fullyParallel: true,
+    forbidOnly: !!process.env.CI,
+    retries: process.env.CI ? 2 : 0,
+    workers: process.env.CI ? 1 : undefined,
+    reporter: "html",
+    use: {
+        trace: "on-first-retry",
     },
-    {
-      name: "firefox",
-      use: { ...devices["Desktop Firefox"] },
-    },
-    {
-      name: "webkit",
-      use: { ...devices["Desktop Safari"] },
-    },
-  ],
-  webServer: [
-    {
-      command: "pnpm --filter jamesdurban.com dev",
-      url: "http://localhost:35421",
-      timeout: 120 * 1000,
-      reuseExistingServer: !process.env.CI,
-    },
-    {
-      command: "pnpm --filter blackcattattoos.co.uk dev",
-      url: "http://localhost:35422",
-      timeout: 120 * 1000,
-      reuseExistingServer: !process.env.CI,
-    },
-    {
-      command: "pnpm --filter orchitect.net dev",
-      url: "http://localhost:35423",
-      timeout: 120 * 1000,
-      reuseExistingServer: !process.env.CI,
-    },
-    {
-      command: "pnpm --filter stcatherinesgroup.com dev",
-      url: "http://localhost:35424",
-      timeout: 120 * 1000,
-      reuseExistingServer: !process.env.CI,
-    },
-    {
-      command: "pnpm --filter thecontourclinicrichmond.co.uk dev",
-      url: "http://localhost:35425",
-      timeout: 120 * 1000,
-      reuseExistingServer: !process.env.CI,
-    },
-  ],
+    projects: [
+        {
+            name: "chromium",
+            use: {...devices["Desktop Chrome"]},
+        },
+        {
+            name: "webkit",
+            use: {...devices["Desktop Safari"]},
+        },
+    ],
+    webServer: [
+        {
+            command: "pnpm --filter jamesdurban.com dev",
+            url: "http://localhost:35421",
+            timeout: 120 * 1000,
+            reuseExistingServer: !process.env.CI,
+        },
+        {
+            command: "pnpm --filter blackcattattoos.co.uk dev",
+            url: "http://localhost:35422",
+            timeout: 120 * 1000,
+            reuseExistingServer: !process.env.CI,
+        },
+        {
+            command: "pnpm --filter orchitect.net dev",
+            url: "http://localhost:35423",
+            timeout: 120 * 1000,
+            reuseExistingServer: !process.env.CI,
+        },
+        {
+            command: "pnpm --filter stcatherinesgroup.com dev",
+            url: "http://localhost:35424",
+            timeout: 120 * 1000,
+            reuseExistingServer: !process.env.CI,
+        },
+        {
+            command: "pnpm --filter thecontourclinicrichmond.co.uk dev",
+            url: "http://localhost:35425",
+            timeout: 120 * 1000,
+            reuseExistingServer: !process.env.CI,
+        },
+    ],
 });
