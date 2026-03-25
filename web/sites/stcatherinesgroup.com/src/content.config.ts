27f798bf3b3c355de9661790e167ea9eb6bc621e
james <james_d02@protonmail.com>
2026-03-25 17:38:44 +0000

    format

--- web/sites/stcatherinesgroup.com/src/content.config.ts
+++ web/sites/stcatherinesgroup.com/src/content.config.ts
@@ -1,19 +1,19 @@
-import {defineCollection} from "astro:content";
-import {glob} from "astro/loaders";
-import {z} from "astro/zod"
+import { defineCollection } from "astro:content";
+import { glob } from "astro/loaders";
+import { z } from "astro/zod";
 
 const pages = defineCollection({
-    loader: glob({pattern: "**/*.md", base: "./src/content/pages"}),
-    schema: z.object({
-        title: z.string(),
-        description: z.string(),
-        hero: z.object({
-            eyebrow: z.string(),
-            heading: z.string(),
-            subheading: z.string(),
-        }),
-        pullQuote: z.string().optional()
+  loader: glob({ pattern: "**/*.md", base: "./src/content/pages" }),
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    hero: z.object({
+      eyebrow: z.string(),
+      heading: z.string(),
+      subheading: z.string(),
     }),
+    pullQuote: z.string().optional(),
+  }),
 });
 
-export const collections = {pages};
+export const collections = { pages };
